33420ccbe4f2217f742daaf341f9044bce5e304e
Morgan Persson <lazydev@gmail.com>
2011-05-08 23:46:07 +0200

    problem 3 solved

---
+++ Euler/3.fsx
@@ -0,0 +1,10 @@
+﻿let num = 600851475143L
+
+let findLargestPrime n =
+    let rec largestPrime n d ds =
+        match n with
+        | 1L -> Seq.max ds
+        | x -> if (n%d)=0L then (largestPrime (n/d) 2L (d::ds)) else largestPrime n (d+1L) ds
+    largestPrime n 2L [1L]
+
+printfn "%d" (findLargestPrime num) 
