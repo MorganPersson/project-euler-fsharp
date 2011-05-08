271231eff670f9a007d9fc00a5d6d22bbbef48c7
Morgan Persson <lazydev@gmail.com>
2011-05-08 23:45:59 +0200

    problem 2 solved

---
+++ Euler/2.fsx
@@ -0,0 +1,7 @@
+﻿let rec fib l = 
+    match l with
+        |[] -> fib [2;1]
+        | h::t when h < 4000000 -> fib ((h + (Seq.nth 0 t)) :: l)
+        | h::t -> Seq.filter (fun f -> (f%2)=0) t |> Seq.sum 
+
+printfn "1: %d" (fib [])  
