58af0db806f1832913f1a9cb5e1a7c6ed53b70d0
Morgan Persson <lazydev@gmail.com>
2011-05-09 09:03:02 +0200

    problem 4 solved

---
+++ Euler/4.fsx
@@ -0,0 +1,18 @@
+﻿open System
+open System.Collections.Generic
+
+let findLargestPalindrom =
+    let a = [100..999]
+    let b = [100..999]
+    
+    let reverse i = 
+        Int32.Parse(new string (i.ToString().ToCharArray() |> Array.rev))
+
+    let x = [ for n in a do
+                for m in b do
+                    yield n*m ]
+
+    let l = List.filter (fun f -> (f = reverse f)) x |> List.sort |> List.rev
+    List.nth l 0
+
+printfn "%d" (findLargestPalindrom) 
