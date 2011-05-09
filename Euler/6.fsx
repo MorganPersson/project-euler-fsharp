607af38bf783d157984b39ca7bdd909a10590818
Morgan Persson <lazydev@gmail.com>
2011-05-09 22:59:39 +0200

    problem 6 solved

---
+++ Euler/6.fsx
@@ -0,0 +1,6 @@
+﻿let square i = i*i
+let sumOfSquares = [1..100] |> Seq.map square |> Seq.sum
+let squareOfSum = [1..100] |> Seq.sum |>  square
+
+let diff = squareOfSum - sumOfSquares
+printfn "%d - %d = %d" squareOfSum sumOfSquares diff
