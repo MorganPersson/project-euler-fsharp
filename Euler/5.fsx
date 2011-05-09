44eee795235a397176edf28c0a3dc55a9ea34aff
Morgan Persson <lazydev@gmail.com>
2011-05-09 22:51:50 +0200

    problem #5 solved

---
+++ Euler/5.fsx
@@ -0,0 +1,15 @@
+﻿open System
+open System.Diagnostics
+
+let answer = 2*2*2*2*3*3*5*7*11*13*17*19
+
+let rec isDivisibleByAllBelow i div = 
+    match div with
+        | 1 -> true
+        | x when(i%div)>0 -> false
+        | _ -> isDivisibleByAllBelow i (div-1)
+
+let f = Seq.initInfinite (fun i -> 20 + 2*i) |> Seq.skipWhile (fun f -> (isDivisibleByAllBelow f 20)=false) |> Seq.nth 0
+
+printfn "%d" (f)
+printfn "%d" (answer)
