a46790226ea28f643659d6d7c6220221047fea6a
Morgan Persson <lazydev@gmail.com>
2011-05-13 00:06:36 +0200

    problem 14 solved

---
+++ Euler/14.fsx
@@ -0,0 +1,37 @@
+﻿open System
+
+let even n = n/2L
+let odd n = 3L*n + 1L
+
+let printList l = 
+    l |> List.iter (fun f-> printf "%d ->" f)
+    printfn ""
+
+let buildSequence n = 
+    let rec f n acc = 
+        let func = if (n%2L)=0L then even else odd
+        if (n=1L) then 
+            let result = (n::acc)
+            result
+        else
+            let nn = func n
+            if (nn<0L) then System.Diagnostics.Debugger.Break |> ignore
+            f nn (n::acc)
+    f n []
+
+let longest = let rec f n acc =
+                    let s = buildSequence n
+                    if (n=1000000L) then
+                        acc
+                    else
+                        let newAcc = if (List.length s > List.length acc) then 
+                                        printfn "%d %d" n (List.length s)
+                                        List.rev s 
+                                        else 
+                                        acc
+                        f (n+1L) newAcc
+              f 2L []
+
+
+printList longest 
+printfn "%d" (List.length longest)
