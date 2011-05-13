c484a0a79df53df8deffe736d1b86d27820f2827
Morgan Persson <lazydev@gmail.com>
2011-05-13 17:52:17 +0200

    problem 22 solved

---
+++ Euler/22.fsx
@@ -0,0 +1,27 @@
+﻿open System.IO
+open System
+
+let fileName = @"C:\code\F#\Euler\Euler\names.txt"
+
+let loadFile f =
+    let p = Path.GetFullPath(fileName) 
+    let text = File.ReadAllText(fileName)
+    let txt = text.Replace("\"", "")
+    txt.Split ([|','|])
+    |> List.ofArray
+
+let toBytes s = 
+    System.Text.Encoding.ASCII.GetBytes(s.ToString())  |> List.ofArray
+    
+let nameValue s = 
+    toBytes s 
+        |> List.map (fun b -> (int)b - 64) 
+        |> List.sum
+
+let result = 
+    loadFile fileName
+            |> List.sort 
+            |> List.mapi (fun i s -> (i+1) * (nameValue s)) 
+            |> List.sum
+
+printfn "%d" result
