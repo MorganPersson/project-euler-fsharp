8bc84568be5edf771bc5384b83f1860f4e0b3850
Morgan Persson <lazydev@gmail.com>
2011-05-13 21:59:39 +0200

    problem 16 solved with System.Numerics, way to easy :)

---
+++ Euler/16.fsx
@@ -0,0 +1,20 @@
+﻿open System.Numerics
+open System
+
+let ten = new BigInteger(10)
+
+let sum v = 
+    let rec sum i acc =
+        if (i=BigInteger.Zero) then
+            acc
+        else
+            let newAcc = acc + (i % ten)
+            let ni = i / ten
+            sum ni newAcc 
+
+    sum v BigInteger.Zero
+
+let two = new BigInteger(2)
+let f = sum (two**1000)
+printfn "%s" (f.ToString())
+
