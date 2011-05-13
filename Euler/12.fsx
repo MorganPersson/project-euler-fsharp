ac5b7a1bb46d8388096db759ae5898fd59463948
Morgan Persson <lazydev@gmail.com>
2011-05-13 21:18:30 +0200

    problem 12 solved

---
+++ Euler/12.fsx
@@ -0,0 +1,32 @@
+﻿open System
+
+let nextTriangleNumber n = 
+    let i = fst n + 1
+    let v = snd n
+    (i, i + v)
+    
+let triangleNumbers i = 
+    [1..i] |> List.mapi (fun i f -> ((i+1), f + (i+1)))
+
+let factors i = 
+    let max = (int (sqrt(float i))) + 1
+    let pairsToBuild = [2..max]
+    [   yield 1 
+        yield i
+        for n in [2..max] do
+        if (i%n)=0 then 
+            yield n
+            yield i/n]
+
+
+let five01 =
+    let rec repeat n =
+        let i = snd n
+        let nums = factors i
+        if (Seq.length nums > 500) then
+            i
+        else
+            repeat (nextTriangleNumber n)
+    repeat (1,1)
+
+printfn "%d" (five01)
