0f8b631eb254da79ffd6328e563135717597d3a6
Morgan Persson <lazydev@gmail.com>
2011-05-13 21:49:23 +0200

    problem 20 solved

---
+++ Euler/20.fsx
@@ -0,0 +1,25 @@
+﻿open System
+open System.Numerics
+
+let rec fac n =
+    if (n=BigInteger.One) then
+        BigInteger.One
+    else
+        let a =  n * fac (n-BigInteger.One)
+        a
+            
+let ten = new BigInteger(10)
+
+let sum = 
+    let v = fac (new BigInteger(100))
+    let rec sum i acc =
+        if (i=BigInteger.Zero) then
+            acc
+        else
+            let newAcc = acc + (i % ten)
+            let ni = i / ten
+            sum ni newAcc 
+
+    sum v BigInteger.Zero
+
+printfn "%s" (sum.ToString())
