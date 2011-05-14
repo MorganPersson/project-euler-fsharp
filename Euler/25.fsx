9aa8a1f40bb2f6de72eae01d5ee1df51c333ebd2
Morgan Persson <lazydev@gmail.com>
2011-05-14 22:55:22 +0200

    problem 25 solved

---
+++ Euler/25.fsx
@@ -0,0 +1,14 @@
+﻿open System.Numerics
+
+let stop = BigInteger.Pow(new BigInteger(10),999)
+let fib = 
+    let rec fib a b term = 
+        let n = a + b
+        if (n>=stop)
+            then term
+        else
+            fib b n (term+1) 
+    fib (BigInteger.One) (BigInteger.One) 3
+
+printfn "%d" (stop.ToString().Length)
+printfn "%s" (fib.ToString())
