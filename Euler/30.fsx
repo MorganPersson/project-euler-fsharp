ddf9fd3dda8047779701c8d5d147546772c0d940
Morgan Persson <lazydev@gmail.com>
2011-05-15 13:08:56 +0200

    problem 30 solved

---
+++ Euler/30.fsx
@@ -0,0 +1,34 @@
+﻿    open System
+
+    let pow n (exp:int) = int (float n ** float exp)
+
+    let sumOfParts n p =
+        let rec sumOfParts n acc =  
+            if (n=0) then
+                acc
+            else
+                let reminder = n%10
+                let nn = n/10
+                sumOfParts nn (reminder::acc)
+        let sum = sumOfParts n []
+        sum |> List.map (fun f -> pow f p) |> List.sum
+    
+
+    let problem p = 
+        let max = p*(pow 9 p)
+        let numbersToCheck = [for n in [2..max] do yield n]
+        let rec problem nums acc =
+            match nums with
+                | [] -> List.sort acc
+                | h::t ->
+                            let s = sumOfParts h p
+                            if (h=s) then
+                                let nAcc = s::acc
+                                problem t nAcc
+                            else
+                                problem t acc
+
+        problem numbersToCheck [] |> List.sum
+
+    let sum = problem 5
+    printfn "sum: %d" sum
